a110ed3e74ba723c085244ba19cc389bf6b5218f
Shaun Dashjian <shaundashjian@yahoo.com>
2018-12-18 00:21:00 -0700

    init commit

---
+++ src/test/java/com/shaundashjian/nicebank/Stepdefs.java
@@ -0,0 +1,10 @@
+package com.shaundashjian.nicebank;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+}
